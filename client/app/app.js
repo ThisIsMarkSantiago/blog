10e644403a537f4468496ec457a8b261b73643e3
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-12-01 16:50:03 +0800

    Initial Setup

---
+++ client/app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myblogApp', ['myblogApp.auth', 'myblogApp.admin', 'myblogApp.constants',
+    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
+    'validation.match'
+  ])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
