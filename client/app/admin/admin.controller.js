b994da43f093d8bcf31b7dd44eb305913a39ada2
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-12-13 17:00:59 +0800

    Admin Post Editor

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -4,7 +4,32 @@
 
   class AdminController {
     constructor(Auth) {
-      this.user = Auth.getCurrentUser();
+
+      angular.extend( this, {
+        user: Auth.getCurrentUser(),
+        post: {}
+      });
+    }
+
+    preview() {
+      return [
+        '<style type="text/css">',
+        this.post.style,
+        '</style>',
+        '<h1>',
+        this.post.title,
+        '</h1>',
+        this.post.markup,
+        '<script type="text/javascript">',
+        this.post.script,
+        '</script>'
+      ].join('');
+    }
+
+    submit(form) {
+      if (form.$valid) {
+        // TODO
+      }
     }
   }
 
