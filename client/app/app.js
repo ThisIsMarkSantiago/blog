dd8ee298aeeb1c76d9646da049f05734d6b4a1d0
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-12-13 12:58:40 +0800

    Bower install infinite-scroll

--- client/app/app.js
+++ client/app/app.js
@@ -10,7 +10,8 @@ angular.module('myblogApp', [
     'btford.socket-io',
     'ui.router',
     'ui.bootstrap',
-    'validation.match'
+    'validation.match',
+    'infinite-scroll'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
